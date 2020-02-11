bf37d03dc404d46f91bfbb2e0dac47a04549ee79
Олег Кувшинов <okuvshinov@inbox.ru>
2020-02-11 19:26:19 +0500

    add comments

---
+++ NewClipping/NewClipping.h
@@ -0,0 +1,32 @@
+﻿
+// NewClipping.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CNewClippingApp:
+// See NewClipping.cpp for the implementation of this class
+//
+
+class CNewClippingApp : public CWinApp
+{
+public:
+	CNewClippingApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CNewClippingApp theApp;
---
+++ NewClipping/NewClippingDlg.h
@@ -0,0 +1,33 @@
+﻿
+// NewClippingDlg.h : header file
+//
+
+#pragma once
+
+
+// CNewClippingDlg dialog
+class CNewClippingDlg : public CDialogEx
+{
+// Construction
+public:
+	CNewClippingDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_NEWCLIPPING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
